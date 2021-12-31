3ceb6817f5a779550c2a0c1d521499f5266816bf
thsnyzkn <tahsinyazkan@gmail.com>
2021-12-31 14:27:51 +0300

    added apollo and graphql package, queryed characters list.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+const CHARACTERS_LIST = gql`
+  query {
+    characters {
+      results {
+        name
+        image
+      }
+    }
+  }
+`;
+function Characters() {
+  const { loading, error, data } = useQuery(CHARACTERS_LIST);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
+  return (
+    <ul>
+      {data?.characters?.results?.map(({ name, image }, index) => (
+        <li
+          key={index}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <span>{name}</span>
+          <img src={image} alt={`${name}'s visual depiction`} />
+        </li>
+      ))}
+    </ul>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Characters />
     </div>
   );
 }
